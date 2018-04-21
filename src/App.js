531c74a08f2d315aed8df2c6be1581ed0cc14c64
Yaoxiang Li <liyaoxiang@outlook.com>
2018-04-21 03:57:58 -0400

    added map component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Map from './Map'
+import './App.css'
 
 class App extends React.Component {
-
   state = {
     map: '',
     info: ''
   }
-
   componentDidMount() {
+    window.initMap = this.initMap
+    // append the Google Map API script as the last child of <body>
     const script = document.createElement("script")
     script.async = true
     script.defer = true
-    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAD-9mxlvsiVnVpdpRQhCBDAoVQdoBrbyM&v=3&callback=initMap"
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAD-9mxlvsiVnVpdpRQhCBDAoVQdoBrbyM&v=3&callback=window.initMap"
     document.body.appendChild(script)
-
-    window.initMap = this.initMap
   }
 
-  initMap() {
+  initMap = () => {
     let map
+    let infoWindow
     map = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: 38.889931, lng: -77.009003 }, // Washington D.C
       zoom: 13,
       mapTypeControl: false
-    });
+    })
 
-    let infoWindow = new window.google.maps.InfoWindow();
+    infoWindow = new window.google.maps.InfoWindow({})
     this.setState({
       map: map,
       info: infoWindow
@@ -35,7 +35,7 @@ class App extends React.Component {
 
   render() {
     return (
-      'This is a dc-reastaurants-map app demo!'
+      <Map />
     )
   }
 }
---
+++ src/Map.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div id="map-container">
+        <div id="map" role="application"></div>
+      </div>
+    )
+  }
+}
+
+export default Map
