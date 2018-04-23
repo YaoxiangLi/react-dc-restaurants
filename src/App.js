75881d7bee8c40bb8d83e06d80119a47c5750f59
Yaoxiang Li <liyaoxiang@outlook.com>
2018-04-23 00:23:32 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from 'react'
 import Map from './Map'
+import ListView from './ListView'
 import './App.css'
 
 class App extends React.Component {
   state = {
     map: '',
-    info: ''
+    infoWindow: '',
+    tags: [
+      {
+        lat: 38.8979725,
+        long: -77.0332762,
+        name: "Old Ebbitt Grill"
+      },
+      {
+        lat: 38.900305,
+        long: -77.0444647,
+        name: "Founding Farmers DC"
+      },
+      {
+        lat: 38.8987445,
+        long: -77.023684,
+        name: "Zaytinya"
+      },
+      {
+        lat: 38.9113639,
+        long: -77.0315724,
+        name: "Le Diplomate"
+      },
+      {
+        lat: 38.9022395,
+        long: -77.0244737,
+        name: "Acadiana"
+      },
+      {
+        lat: 38.9018414,
+        long: -77.0332468,
+        name: "Siroc Restaurant"
+      },
+    ],
+    markers: []
   }
+
   componentDidMount() {
     window.initMap = this.initMap
-    // append the Google Map API script as the last child of <body>
-    const script = document.createElement("script")
+    let script = document.createElement("script")
     script.async = true
     script.defer = true
     script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAD-9mxlvsiVnVpdpRQhCBDAoVQdoBrbyM&v=3&callback=window.initMap"
@@ -18,24 +52,96 @@ class App extends React.Component {
   }
 
   initMap = () => {
-    let map
-    let infoWindow
-    map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 38.889931, lng: -77.009003 }, // Washington D.C
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 38.8979725, lng: -77.0332762 }, // Washington D.C
       zoom: 13,
       mapTypeControl: false
     })
 
-    infoWindow = new window.google.maps.InfoWindow({})
+    const infoWindow = new window.google.maps.InfoWindow({})
     this.setState({
       map: map,
-      info: infoWindow
+      infoWindow: infoWindow
+    })
+    this.generateMarkers(map)
+  }
+
+  generateMarkers = (map) => {
+    let self = this
+    this.state.tags.forEach(marker => {
+      const loc = {lat: marker.lat, lng: marker.long}
+      let mark = new window.google.maps.Marker({
+        position: loc,
+        title: marker.name,
+        animation: window.google.maps.Animation.DROP,
+        map: map
+      })
+      mark.addListener('click', function() {
+        self.showMarker(mark)
+      })
+      let newMakers = this.state.markers
+      newMakers.push(mark)
+      this.setState({markers: newMakers})
+    })
+  }
+
+
+  showMarker = (marker) => {
+    const CLIENT_ID = "WT4UPILVTMA5WNQ0VZ4Z2XJCJSRDZHCME3DHMXAZHOECIOYQ"
+    const CLIENT_SECRET = "MNWOFUZR3IRQ2NXMVNOZQDBD504KKHBHWPCBIETA5U3QXO2Z"
+    const URL =`https://api.foursquare.com/v2/venues/search?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&ll=${marker.getPosition().lat()},${marker.getPosition().lng()}&limit=1`
+    // Check to make sure the infowindow is not already opened on this marker
+    if (this.state.infoWindow.marker !== marker) {
+      this.state.infoWindow.marker = marker
+      // this.setState({infoWindow: {marker: marker}})
+      this.state.infoWindow.open(this.state.map, marker)
+      // marker.setAnimation(window.google.maps.Animation.DROP)
+      this.state.infoWindow.addListener('closeClick', function() {
+        this.state.infoWindow.setMarker(null)
+      })
+      this.markerInfo(URL)
+    }
+  }
+
+  markerInfo(url) {
+    let self = this.state.infoWindow
+    fetch(url)
+    .then(function (res) {
+      if (res.status !== 200) {
+        this.state.infoWindow.setContent("Error loading data from API!")
+      }
+      res.json().then(function (data) {
+        const restaurant = data.response.venues[0]
+        let category, address
+        console.log(data.response.venues[0])
+        if (restaurant.categories[0].name) {
+          category = `<p><b>Categories: </b>${restaurant.categories[0].name}</p>`
+        }
+        if (restaurant.location.formattedAddress) {
+          address = `<p><b>Address: </b> ${restaurant.location.formattedAddress}</p>`
+        }
+        let info = `<div id='marker'><h2>${self.marker.title}</h2>${category}${address}</div>`
+        self.setContent(info)
+      })
+    })
+    .catch(function(e) {
+      self.setContent("Error loading data from API!")
     })
   }
 
   render() {
     return (
-      <Map />
+      <div>
+        <header>
+          <ListView
+            markers={ this.state.markers }
+            showMarker={ this.showMarker }
+            infoWindow={ this.state.infoWindow }
+          />
+          <h1 id="title">Restaurants in D.C.</h1>
+        </header>
+        <Map />
+      </div>
     )
   }
 }
--- src/ListView.js
+++ src/ListView.js
@@ -1,15 +1,73 @@
 import React, { Component } from 'react'
 
 class ListView extends Component {
+  state = {
+    query: '',
+    info: '',
+    markers: []
+  }
+
+  componentDidMount() {
+    this.setState({markers: this.props.markers})
+  }
+
+  showSideBar = () => {
+    const sideBar = document.querySelector('.sideBar');
+    if (sideBar.style.display === 'none') {
+      sideBar.style.display = 'block'
+    } else {
+      sideBar.style.display = 'none'
+    }
+  }
+
+  search = (e) => {
+    const query = e.target.value.toLowerCase()
+    const markers = this.props.markers
+    const newMarkers = []
+
+    markers.forEach((marker) => {
+      if (marker.title.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+        marker.setVisible(true)
+        newMarkers.push(marker)
+      } else {
+        marker.setVisible(false)
+      }
+    })
+    this.setState({markers: newMarkers})
+  }
+
   render() {
+    const { markers } = this.state
+    const { showMarker } = this.props
     return (
-      <ul>
-        <li>Restaurant 1</li>
-        <li>Restaurant 2</li>
-        <li>Restaurant 3</li>
-        <li>Restaurant 4</li>
-        <li>Restaurant 5</li>
-      </ul>
+      <div>
+        <div className="veggieburgers" onClick={this.showSideBar}>
+          <div className="veggieline"></div>
+          <div className="veggieline"></div>
+          <div className="veggieline"></div>
+        </div>
+        <div className="sideBar">
+          <div className="form" role="form">
+            <input type="text"
+              aria-labelledby="filter" placeholder="Search..."
+              className="input" role="search"
+              onChange={this.search}/>
+          </div>
+          <ul>
+            {markers && markers.map((marker, index) =>
+              <li key={ index }>
+                <a
+                  onClick={ showMarker.bind(this, marker) }
+                  tabIndex={ index }
+                  role="button">{ marker.title }
+                </a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
     )
   }
 }
+
+export default ListView;
